6eda968f9949b2e8536524907d475752741cfe43
Jake Kinchen <jake.kinchen@onetier.com>
2024-08-15 08:39:47 -0500

    path fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,27 @@ import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
 import node from '@astrojs/node';
 
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
 // https://astro.build/config
 export default defineConfig({
   //output: 'hybrid',
   //adapter: node({
   //  mode: 'standalone'
   //}),
+  vite: {
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src')
+      },
+      extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.d.ts']
+    }
+  },
   outDir: './dist',
   site: 'https://example.com',
   integrations: [
